3d35173a50f79158a54a41492fa1176ff0148c2e
Kirill Klenov <horneds@gmail.com>
2012-07-09 14:38:13 +0400

    Change Admin Views.

--- base/admin.py
+++ base/admin.py
@@ -4,15 +4,20 @@ from flask.ext.admin.contrib.sqlamodel import ModelView
 from base.users import permission
 
 
-class AuthAdminIndexView(AdminIndexView):
+class StaffAdminView(AdminIndexView):
+    " Staff admin home page. "
     def is_accessible(self):
         return permission.staff.can()
 
 
 class AuthModelView(ModelView):
+    def __init__(self, *args, **kwargs):
+        self.permission = kwargs.pop('permission', None) or permission.admin
+        super(AuthModelView, self).__init__(*args, **kwargs)
+
     def is_accessible(self):
-        return permission.admin.can()
+        return self.permission.can()
 
 
 # Create admin
-admin = Admin(name='Admin', index_view=AuthAdminIndexView())
+admin = Admin(name='Admin', index_view=StaffAdminView())
