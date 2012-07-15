27561b8e508f220d4f12a3e1433076193fcb6631
Kirill Klenov <horneds@gmail.com>
2012-07-16 00:58:49 +0400

    Update settings

--- base/settings.py
+++ base/settings.py
@@ -5,25 +5,29 @@ __basedir__ = op.abspath(op.dirname(op.dirname(__file__)))
 
 
 class Config(object):
-    DEBUG = False
-    TESTING = False
+
+    # Database
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + op.join(__basedir__, '.db')
+
+    # WTForms
     CSRF_ENABLED = True
     CSRF_SESSION_KEY = "somethingimpossibletoguess"
+
+    # Babel
     BABEL_LANGUAGES = ['en', 'ru']
+    BABEL_DEFAULT_LOCALE = 'en'
 
 
 class Production(Config):
     ADMINS = frozenset(['youremail@yourdomain.com'])
     SECRET_KEY = 'SecretKeyForSessionSigning'
-    DATABASE_CONNECT_OPTIONS = {}
-    BABEL_DEFAULT_LOCALE = 'en'
 
 
 class Testing(Production):
     TESTING = True
-    CSRF_ENABLED = False
     SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
+    CSRF_ENABLED = False
+    CACHE_TYPE = 'null'
 
 
 class Develop(Production):
