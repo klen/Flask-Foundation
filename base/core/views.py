66069932d6d448a9fefa74decfe905b5bf3545fa
Kirill Klenov <horneds@gmail.com>
2012-09-29 17:20:05 +0400

    Add Foundation logo.

--- base/core/views.py
+++ base/core/views.py
@@ -3,7 +3,10 @@ from flask import render_template, Blueprint, current_app
 from ..auth.forms import LoginForm
 
 
-core = Blueprint('core', __name__, template_folder='templates')
+core = Blueprint('core', __name__,
+                 template_folder='templates',
+                 static_url_path='/static/core',
+                 static_folder='static')
 
 
 if current_app and current_app.config.get('AUTH_LOGIN_VIEW') == 'core.index':
