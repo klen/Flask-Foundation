ca674b743b6d48593f45d999335ae893cf2a90d6
Kirill Klenov <horneds@gmail.com>
2012-09-29 18:39:27 +0400

    Add github and facebook oauth credentials.

--- base/config/production.py
+++ base/config/production.py
@@ -14,9 +14,18 @@ COLLECT_STATIC_ROOT = op.join(op.dirname(ROOTDIR), 'static')
 # auth.oauth
 # ----------
 OAUTH_TWITTER = dict(
-    # flask-base-template app
-    consumer_key='ydcXz2pWyePfc3MX3nxJw',
-    consumer_secret='Pt1t2PjzKu8vsX5ixbFKu5gNEAekYrbpJrlsQMIwquc'
+    consumer_key='750sRyKzvdGPJjPd96yfgw',
+    consumer_secret='UGcyjDCUOb1q44w1nUk8FA7aXxvwwj1BCbiFvYYI',
+)
+
+OAUTH_FACEBOOK = dict(
+    consumer_key='413457268707622',
+    consumer_secret='48e9be9f4e8abccd3fb916a3f646dd3f',
+)
+
+OAUTH_GITHUB = dict(
+    consumer_key='8bdb217c5df1c20fe632',
+    consumer_secret='a3aa972b2e66e3fac488b4544d55eda2aa2768b6',
 )
 
 # dealer
