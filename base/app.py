3e0a9eeadcfb3caa5c22bb8d12a62b3eba301208
Kirill Klenov <horneds@gmail.com>
2012-10-02 19:36:03 +0400

    Force local settings

--- base/app.py
+++ base/app.py
@@ -1,17 +1,23 @@
 from flask import Flask
-from os import environ
+from os import environ, path as op
 
 from .config import production
 
 
 def create_app(config=None, **settings):
     app = Flask(__name__)
-
-    app.config.from_object(config or production)
     app.config.from_envvar("APP_SETTINGS", silent=True)
+    app.config.from_object(config or production)
+
+    # Settings from mode
     mode = environ.get('MODE')
     if mode:
         app.config.from_object('base.config.%s' % mode)
+
+    # Local settings
+    app.config.from_pyfile(op.join(op.dirname(production.__file__), 'local.py'), silent=True)
+
+    # Overide settings
     app.config.update(settings)
 
     with app.test_request_context():
